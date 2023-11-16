0de929053b45e35a040ad9a17d0fe264d9d298b2
Oleh Melenevskyi <oleh.melenevskyi@ext.runpanther.io>
2023-11-16 14:35:14 +0200

    Refactored tests for SdYamlTransformation

--- sigma/pipelines/panther/panther_sdyaml_pipeline.py
+++ sigma/pipelines/panther/panther_sdyaml_pipeline.py
@@ -1,54 +1,8 @@
-import logging
-import uuid
-from os import path
-from typing import Any
 from sigma.pipelines.common import logsource_windows_process_creation
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
 from sigma.processing.transformations import FieldMappingTransformation
-from sigma.processing.postprocessing import QueryPostprocessingTransformation
-from sigma.rule import SigmaRule
 
-LOG_TYPES_MAPPING = {
-    "windows": "Windows.EventLogs",
-}
-
-
-class SdYamlTransformation(QueryPostprocessingTransformation):
-    identifier = "SDYaml"
-
-    def apply(self, pipeline: ProcessingPipeline, rule: SigmaRule, query: Any) -> Any:
-        res = {
-            "AnalysisType": "rule",
-            "DisplayName": rule.title,
-            "Description": rule.description,
-            "Tags": [tag.name for tag in rule.tags],
-            "Enabled": True,
-            "Detection": [query],
-        }
-
-        if rule.references:
-            res["Reference"] = ", ".join(rule.references)
-
-        if rule.author:
-            res["Description"] += f"\n\nAuthor: {rule.author}"
-
-        rule_id = rule.id or uuid.uuid4()
-        res["RuleID"] = str(rule_id)
-
-        if rule.source:
-            res["SigmaFile"] = path.split(rule.source.path)[-1]
-
-        if rule.level:
-            res["Severity"] = rule.level.name
-
-        key = rule.logsource.product
-        log_type = LOG_TYPES_MAPPING.get(rule.logsource.product)
-        if log_type is None:
-            logging.error(f"Can't find LogTypes mapping for {key}")
-        else:
-            res["LogTypes"] = [log_type]
-
-        return res, True
+from sigma.pipelines.panther.sdyaml_transformation import SdYamlTransformation
 
 
 def panther_sdyaml_pipeline():
