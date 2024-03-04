7d4da0dab9c81a2c185bc3470114eff39b6eab75
Oleh Melenevskyi <767472+melenevskyi@users.noreply.github.com>
2024-03-04 09:02:48 +0200

    Replaced Crowdstrike dependency with plain code (#28)

--- sigma/pipelines/panther/crowdstrike_panther_pipeline.py
+++ sigma/pipelines/panther/crowdstrike_panther_pipeline.py
@@ -1,10 +1,18 @@
-from sigma.pipelines.crowdstrike import crowdstrike_fdr_pipeline
-from sigma.processing.conditions import IncludeFieldCondition
-from sigma.processing.pipeline import ProcessingItem
+from sigma.pipelines.common import (
+    logsource_windows_dns_query,
+    logsource_windows_network_connection,
+    logsource_windows_network_connection_initiated,
+    logsource_windows_process_creation,
+)
+from sigma.processing.conditions import IncludeFieldCondition, MatchStringCondition
+from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
 from sigma.processing.transformations import (
     AddConditionTransformation,
+    ChangeLogsourceTransformation,
+    DetectionItemFailureTransformation,
     DropDetectionItemTransformation,
     FieldMappingTransformation,
+    ReplaceStringTransformation,
 )
 
 from sigma.pipelines.panther.panther_sdyaml_pipeline import (
@@ -19,117 +27,273 @@ from sigma.pipelines.panther.sdyaml_transformation import SdYamlTransformation
 
 
 def crowdstrike_panther_pipeline():
-    crowdstrike_pipeline = crowdstrike_fdr_pipeline()
-    crowdstrike_pipeline.postprocessing_items.append(SdYamlTransformation())
-
-    crowdstrike_pipeline.items += [
-        ProcessingItem(
-            transformation=AddConditionTransformation(
-                {
-                    "event_platform": "Windows",
-                }
-            ),
-            rule_conditions=[
-                logsource_windows(),
-            ],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation(
-                {
-                    "event_platform": "Mac",
-                }
-            ),
-            rule_conditions=[logsource_mac()],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation(
-                {
-                    "event_platform": "Linux",
-                }
-            ),
-            rule_conditions=[logsource_linux()],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation(
-                {
-                    "event_simpleName": "FileOpenInfo",
-                }
-            ),
-            rule_conditions=[logsource_file_event()],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation(
-                {
-                    "event_simpleName": [
-                        "NetworkConnectIP4",
-                        "NetworkConnectIP6",
-                        "NetworkReceiveAcceptIP4",
-                        "NetworkReceiveAcceptIP6",
-                    ],
-                }
-            ),
-            rule_conditions=[logsource_network_connection()],
-        ),
-        ProcessingItem(
-            transformation=FieldMappingTransformation(
-                {
-                    "sha256": "event.SHA256HashData",
-                    "sha1": "event.SHA1HashData",
-                    "ParentImage": "event.ParentBaseFileName",
-                    "Image": "event.ImageFileName",
-                    "CommandLine": "event.CommandLine",
-                    "md5": "event.MD5HashData",
-                    "TargetFileName": "event.TargetFileName",
-                }
-            ),
-        ),
-        ProcessingItem(
-            transformation=DropDetectionItemTransformation(),
-            field_name_conditions=[IncludeFieldCondition(fields=["DestinationHostname"])],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation({"event.Protocol": "ICMP"}),
-            rule_conditions=[
-                RuleIContainsDetectionItemCondition(
-                    field="Protocol",
-                    value=1,
+    crowdstrike_pipeline = ProcessingPipeline(
+        name="Generic Log Sources to CrowdStrike Falcon Data Replicator (FDR) Transformation",
+        priority=10,
+        postprocessing_items=[SdYamlTransformation()],
+        items=[
+            ProcessingItem(
+                identifier="cs_process_creation_eventtype",
+                transformation=AddConditionTransformation(
+                    {
+                        "event_simpleName": ["ProcessRollup2", "SyntheticProcessRollup2"],
+                    }
                 ),
-            ],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation({"event.Protocol": "TCP"}),
-            rule_conditions=[
-                RuleIContainsDetectionItemCondition(
-                    field="Protocol",
-                    value=6,
+                rule_conditions=[
+                    logsource_windows_process_creation(),
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_process_creation_fieldmapping",
+                transformation=FieldMappingTransformation(
+                    {
+                        "Image": "ImageFileName",
+                        "ParentImage": "ParentBaseFileName",
+                    }
                 ),
-            ],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation({"event.Protocol": "UDP"}),
-            rule_conditions=[
-                RuleIContainsDetectionItemCondition(
-                    field="Protocol",
-                    value=17,
+                rule_conditions=[
+                    logsource_windows_process_creation(),
+                ],
+            ),
+            ProcessingItem(
+                identifier="crowdstrike_process_creation_logsource",
+                transformation=ChangeLogsourceTransformation(
+                    category="process_creation",
+                    product="windows",
+                    service="crowdstrike",
                 ),
-            ],
-        ),
-        ProcessingItem(
-            transformation=AddConditionTransformation({"event.Protocol": "IPv6-ICMP"}),
-            rule_conditions=[
-                RuleIContainsDetectionItemCondition(
-                    field="Protocol",
-                    value=58,
+                rule_conditions=[
+                    logsource_windows_process_creation(),
+                ],
+            ),
+            # Network Connection
+            ProcessingItem(
+                identifier="cs_network_connection_eventtype",
+                transformation=AddConditionTransformation(
+                    {
+                        "event_simpleName": "NetworkConnectionIP4",
+                    }
                 ),
-            ],
-        ),
-        ProcessingItem(
-            transformation=DropDetectionItemTransformation(),
-            field_name_conditions=[
-                IncludeFieldCondition(
-                    fields=["Protocol"],
-                )
-            ],
-        ),
-    ]
+                rule_conditions=[
+                    logsource_windows_network_connection(),
+                    logsource_windows_network_connection_initiated(True),
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_network_connection_eventtype",
+                transformation=AddConditionTransformation(
+                    {
+                        "event_simpleName": "NetworkReceiveAcceptIP4",
+                    }
+                ),
+                rule_conditions=[
+                    logsource_windows_network_connection(),
+                    logsource_windows_network_connection_initiated(False),
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_network_connection_fieldmapping",
+                transformation=FieldMappingTransformation(
+                    {
+                        "DestinationIp": "RemoteAddressIP4",
+                        "DestinationPort": "RemotePort",
+                    }
+                ),
+                rule_conditions=[
+                    logsource_windows_network_connection(),
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_network_connection_drop_initiated",
+                transformation=DropDetectionItemTransformation(),
+                rule_conditions=[
+                    logsource_windows_network_connection(),
+                ],
+                field_name_conditions=[
+                    IncludeFieldCondition(fields=["Initiated"]),
+                ],
+            ),
+            ProcessingItem(
+                identifier="crowdstrike_network_connection_logsource",
+                transformation=ChangeLogsourceTransformation(
+                    category="network_connection",
+                    product="windows",
+                    service="crowdstrike",
+                ),
+                rule_conditions=[
+                    logsource_windows_network_connection(),
+                ],
+            ),
+            # DNS Requests
+            ProcessingItem(
+                identifier="cs_dns_query_eventtype",
+                transformation=AddConditionTransformation(
+                    {
+                        "event_simpleName": "DnsRequest",
+                    }
+                ),
+                rule_conditions=[
+                    logsource_windows_dns_query(),
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_dns_query_fieldmapping",
+                transformation=FieldMappingTransformation(
+                    {
+                        "QueryName": "DomainName",
+                        "QueryResults": "IP4Records",
+                    }
+                ),
+                rule_conditions=[
+                    logsource_windows_dns_query(),
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_dns_query_logsource",
+                transformation=ChangeLogsourceTransformation(
+                    category="dns_query",
+                    product="windows",
+                    service="crowdstrike",
+                ),
+                rule_conditions=[
+                    logsource_windows_dns_query(),
+                ],
+            ),
+            # ParentBaseFileName handling
+            ProcessingItem(
+                identifier="cs_parentbasefilename_fail_completepath",
+                transformation=DetectionItemFailureTransformation(
+                    "Only file name of parent image is available in CrowdStrike events."
+                ),
+                field_name_conditions=[
+                    IncludeFieldCondition(fields=["ParentBaseFileName"]),
+                ],
+                detection_item_conditions=[
+                    MatchStringCondition(
+                        cond="any",
+                        pattern="^\\*\\\\?[^\\\\]+$",
+                        negate=True,
+                    )
+                ],
+            ),
+            ProcessingItem(
+                identifier="cs_parentbasefilename_executable_only",
+                transformation=ReplaceStringTransformation(
+                    regex="^\\*\\\\([^\\\\]+)$",
+                    replacement="\\1",
+                ),
+                field_name_conditions=[
+                    IncludeFieldCondition(fields=["ParentBaseFileName"]),
+                ],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation(
+                    {
+                        "event_platform": "Windows",
+                    }
+                ),
+                rule_conditions=[
+                    logsource_windows(),
+                ],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation(
+                    {
+                        "event_platform": "Mac",
+                    }
+                ),
+                rule_conditions=[logsource_mac()],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation(
+                    {
+                        "event_platform": "Linux",
+                    }
+                ),
+                rule_conditions=[logsource_linux()],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation(
+                    {
+                        "event_simpleName": "FileOpenInfo",
+                    }
+                ),
+                rule_conditions=[logsource_file_event()],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation(
+                    {
+                        "event_simpleName": [
+                            "NetworkConnectIP4",
+                            "NetworkConnectIP6",
+                            "NetworkReceiveAcceptIP4",
+                            "NetworkReceiveAcceptIP6",
+                        ],
+                    }
+                ),
+                rule_conditions=[logsource_network_connection()],
+            ),
+            ProcessingItem(
+                transformation=FieldMappingTransformation(
+                    {
+                        "sha256": "event.SHA256HashData",
+                        "sha1": "event.SHA1HashData",
+                        "ParentImage": "event.ParentBaseFileName",
+                        "Image": "event.ImageFileName",
+                        "CommandLine": "event.CommandLine",
+                        "md5": "event.MD5HashData",
+                        "TargetFileName": "event.TargetFileName",
+                    }
+                ),
+            ),
+            ProcessingItem(
+                transformation=DropDetectionItemTransformation(),
+                field_name_conditions=[IncludeFieldCondition(fields=["DestinationHostname"])],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation({"event.Protocol": "ICMP"}),
+                rule_conditions=[
+                    RuleIContainsDetectionItemCondition(
+                        field="Protocol",
+                        value=1,
+                    ),
+                ],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation({"event.Protocol": "TCP"}),
+                rule_conditions=[
+                    RuleIContainsDetectionItemCondition(
+                        field="Protocol",
+                        value=6,
+                    ),
+                ],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation({"event.Protocol": "UDP"}),
+                rule_conditions=[
+                    RuleIContainsDetectionItemCondition(
+                        field="Protocol",
+                        value=17,
+                    ),
+                ],
+            ),
+            ProcessingItem(
+                transformation=AddConditionTransformation({"event.Protocol": "IPv6-ICMP"}),
+                rule_conditions=[
+                    RuleIContainsDetectionItemCondition(
+                        field="Protocol",
+                        value=58,
+                    ),
+                ],
+            ),
+            ProcessingItem(
+                transformation=DropDetectionItemTransformation(),
+                field_name_conditions=[
+                    IncludeFieldCondition(
+                        fields=["Protocol"],
+                    )
+                ],
+            ),
+        ],
+    )
     return crowdstrike_pipeline
---
+++ tests/test_crowdstrike_panther_pipeline.py
@@ -0,0 +1,70 @@
+import uuid
+
+import yaml
+from sigma.collection import SigmaCollection
+from sigma.processing.resolver import ProcessingPipelineResolver
+
+from sigma.backends.panther import PantherSdyamlBackend
+from sigma.pipelines.panther import crowdstrike_panther_pipeline
+
+
+def test_basic():
+    resolver = ProcessingPipelineResolver({"crowdstrike_panther": crowdstrike_panther_pipeline()})
+    pipeline = resolver.resolve_pipeline("crowdstrike_panther")
+    backend = PantherSdyamlBackend(pipeline)
+
+    rule_id = uuid.uuid4()
+    rule = SigmaCollection.from_yaml(
+        f"""
+        title: Test Title
+        id: {rule_id}
+        description: description
+        logsource:
+            category: process_creation
+            product: macos
+        detection:
+            sel:
+                Field1: "banana"
+                DestinationIp: 127.0.0.1
+                Initiated: "true"
+            condition: sel
+    """
+    )
+
+    expected = yaml.dump(
+        {
+            "Description": "description",
+            "AnalysisType": "rule",
+            "DisplayName": "Test Title",
+            "Enabled": True,
+            "Tags": ["Sigma"],
+            "Detection": [
+                {
+                    "All": [
+                        {
+                            "Condition": "Equals",
+                            "KeyPath": "event_platform",
+                            "Value": "Mac",
+                        },
+                        {
+                            "Condition": "Equals",
+                            "KeyPath": "Field1",
+                            "Value": "banana",
+                        },
+                        {
+                            "Condition": "Equals",
+                            "KeyPath": "DestinationIp",
+                            "Value": "127.0.0.1",
+                        },
+                        {
+                            "Condition": "Equals",
+                            "KeyPath": "Initiated",
+                            "Value": "true",
+                        },
+                    ]
+                }
+            ],
+        }
+    )
+
+    assert backend.convert(rule) == expected
