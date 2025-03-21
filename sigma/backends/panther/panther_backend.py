808a3726926cfa06ccd57e7cc768044fc28020f1
akozlovets098 <anastasiia.kozlovets@ext.runpanther.io>
2025-03-21 12:54:09 +0200

    THREAT-382 pySigma pipeline Sysmon - fix import

--- sigma/backends/panther/panther_backend.py
+++ sigma/backends/panther/panther_backend.py
@@ -365,7 +365,7 @@ class PantherBackend(Backend):
     ):
         import_ipaddress = "import ipaddress\n" if "ipaddress." in query else ""
         import_json = "import json\n" if "json." in query else ""
-        import_re = "import re\n" if bool(re.search("[^a-zA-Z]re\.", query)) else ""
+        import_re = "import re\n" if re.search(r'\bre\.', query) else ""
         empty_strings = "\n\n" if import_ipaddress or import_json or import_re else ""
         query = (
             import_ipaddress
